92c0954ff6ec1269acb310a74ce4e1dab5287300
Dan Crankshaw <dscrankshaw@gmail.com>
2011-03-28 20:24:27 -0400

    Added packet buffering to router code. Mostly still unimplemented though.

--- arp.c
+++ arp.c
@@ -177,6 +177,16 @@ void construct_reply(struct packet_state* ps, const struct sr_arphdr* arp_hdr,
 	memmove(ps->response, new_eth, eth_offset);
 	free(reply);
 	free(new_eth);
-	ps->res_len=eth_offset + sizeof(struct sr_arphdr);
+	ps->res_len += sizeof(struct sr_arphdr);
 	printf("Response was constructed.\n");
+}
+
+struct arp_cache_entry* search_cache(struct packet_state* ps, const uint32_t ip)
+{
+	printf("unimplmented");
+	return NULL;
+}
+void send_request(struct packet_state* ps)
+{
+	printf("unimplmented");
 }
